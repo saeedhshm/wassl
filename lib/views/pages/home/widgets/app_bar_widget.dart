5aa7acb499903ea275f150c19faf7f3947e19bf8
Saeed Hashim <saeedhshm@gmail.com>
2023-06-22 13:39:42 +0300

    update order after approve disapprove

--- lib/views/pages/home/widgets/app_bar_widget.dart
+++ lib/views/pages/home/widgets/app_bar_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../getx_controllers/home/home_controller.dart';
+import '../../../consts_widgets/gradiants.dart';
+import '../../../reusable_widgets/localized_text.dart';
 
 class HomeBarWidget extends StatelessWidget {
 
@@ -11,29 +13,34 @@ class HomeBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).viewPadding.top;
     return  Column(
       children: [
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(
-                top: 0,
-                bottom: 8,
-                left: 16,
-                right: 16
-            ),
-            child: Row(
-              children: [
-                Text('welcome'.tr +' : ' + controller.appController.loginModel.value.user!.fullName,style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600
-                ),),
-                const Spacer(),
-                SizedBox(height: 35,width:35,child: Image.asset('assets/images/settingicons/12.png',color: Colors.white,)),
-              ],
+        Container(
+          width: double.infinity,
+          height: 65 + height,
+          child: Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5),
+              child: Row(
+                children: [
+                  LocalizedText(
+                    'welcome'.tr +' : ' + controller.appController.loginModel.value.user!.fullName,
+                    textStyle: const TextStyle(color: Colors.white, fontSize: 15,fontWeight: FontWeight.w600),
+                  ),
+                  const Spacer(),
+                  SizedBox(height: 35,width:35,child: Image.asset('assets/images/settingicons/12.png',color: Colors.white,)),
+
+                ],
+              ),
             ),
           ),
+          decoration: BoxDecoration(
+            gradient: greenGradiantAppBar,
+          ),
         ),
+
         Container(color: Colors.white,width: double.maxFinite,height: 0.5,),
       ],
     );
